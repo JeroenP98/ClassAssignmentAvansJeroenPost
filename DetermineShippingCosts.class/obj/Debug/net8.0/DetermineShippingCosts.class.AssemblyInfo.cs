47dcb84d82de2116eab0c5c3a54a3db7802d2c72
Jeroen <jeroenpost@upcmail.nl>
2025-01-07 19:10:12 +0100

    added branch coverage action

--- DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
+++ DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027a8545779be29715cb9fa3c657a4cf4143a099")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b65f7f99cda548cdc8c71c0e3097652cbb2727c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
