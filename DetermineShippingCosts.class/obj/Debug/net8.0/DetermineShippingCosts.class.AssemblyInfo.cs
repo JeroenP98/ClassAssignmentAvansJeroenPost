e20a8eff9be5b6558a5cd0b9664a6efcf8414fad
Jeroen <jeroenpost@upcmail.nl>
2025-01-12 10:45:21 +0100

    changed step names

--- DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
+++ DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47dcb84d82de2116eab0c5c3a54a3db7802d2c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8bb0f1812908dd4780b61ebb270b3eb4e3e5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
