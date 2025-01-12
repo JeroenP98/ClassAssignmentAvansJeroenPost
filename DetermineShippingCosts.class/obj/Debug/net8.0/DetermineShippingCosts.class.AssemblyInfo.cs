28120850edfd70c0c681e720d9d5e258f4604491
Jeroen <jeroenpost@upcmail.nl>
2025-01-12 12:17:27 +0100

    spli soapui tests into api and load tests

--- DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
+++ DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20a8eff9be5b6558a5cd0b9664a6efcf8414fad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de74ddc44322d8ac6ee9a7b9b7a8073a299677a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
