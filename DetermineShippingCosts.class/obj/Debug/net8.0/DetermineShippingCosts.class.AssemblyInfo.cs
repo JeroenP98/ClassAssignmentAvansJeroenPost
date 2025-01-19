dc70839290c94c697801d78eeb236a95ec3d08fe
jeroenP98 <jeroenpost@upcmail.nl>
2025-01-19 14:00:36 +0100

    test actions 01

--- DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
+++ DetermineShippingCosts.class/obj/Debug/net8.0/DetermineShippingCosts.class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de74ddc44322d8ac6ee9a7b9b7a8073a299677a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efec7a71e6abd9589c3594513168f160dec605b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetermineShippingCosts.class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
+++ PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d487f5cc1c81a9e59f7c1566662d23876df36300")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efec7a71e6abd9589c3594513168f160dec605b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
